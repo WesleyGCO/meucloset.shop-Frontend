70e99f4ae2ca349d6c2c09b3a933bdc980f1b9f8
Vini Ferrari <vipferrari@hotmail.com>
2023-06-06 22:29:50 -0300

    feature/menu lateral criaçao e estilização

---
+++ Frontend/js/index.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var sidebar = document.getElementById("sidebar");
+    var pageContainer = document.getElementById("pageContainer");
+    var pageOverlay = document.getElementById("pageOverlay");
+
+    function toggleSidebar() {
+        sidebar.classList.toggle("open");
+        pageContainer.classList.toggle("blur");
+    }
+
+    function closeSidebar() {
+        sidebar.classList.remove("open");
+        pageContainer.classList.remove("blur");
+    }
+
+    document.getElementById("menuIcon").addEventListener("click", toggleSidebar);
+    pageOverlay.addEventListener("click", closeSidebar);
+});
