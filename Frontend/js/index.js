c122b8907c6047888004e781788b3fc42f1aad86
magalhaesmth <matheusurbano2002@hotmail.com>
2023-06-27 22:39:28 -0300

    Sessao busca por produtos atualizada e estilizada

--- Frontend/js/index.js
+++ Frontend/js/index.js
@@ -17,3 +17,25 @@ document.addEventListener("DOMContentLoaded", function () {
     pageOverlay.addEventListener("click", closeSidebar);
 });
 
+
+String.prototype.reverse = function () {
+    return this.split('').reverse().join('');
+};
+
+function mascaraMoeda(campo, evento) {
+    var tecla = (!evento) ? window.event.keyCode : evento.which;
+    var valor = campo.value.replace(/[^\d]+/gi, '').reverse();
+    var resultado = "";
+    var mascara = "##.###.###,##".reverse();
+    for (var x = 0, y = 0; x < mascara.length && y < valor.length;) {
+        if (mascara.charAt(x) != '#') {
+            resultado += mascara.charAt(x);
+            x++;
+        } else {
+            resultado += valor.charAt(y);
+            y++;
+            x++;
+        }
+    }
+    campo.value = resultado.reverse();
+}
