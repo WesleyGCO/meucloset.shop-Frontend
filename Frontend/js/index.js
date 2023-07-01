eada9ca9580c956d69a820fa7609881579c388e0
Vini Ferrari <vipferrari@hotmail.com>
2023-06-30 21:30:05 -0300

    Atualizações gerais

--- Frontend/js/index.js
+++ Frontend/js/index.js
@@ -16,3 +16,23 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("menuIcon").addEventListener("click", toggleSidebar);
     pageOverlay.addEventListener("click", closeSidebar);
 });
+
+
+document.addEventListener("DOMContentLoaded", function(){
+    var tela_login = document.getElementById("tela_login");
+    var pageContainer = document.getElementById("pageContainer");
+    var pageOverlay = document.getElementById("pageOverlay");
+    
+    function toggleTelaLogin(){
+        tela_login.classList.toggle("open");
+        pageContainer.classList.toggle("blur");
+    }
+    
+    function closeTelaLogin() {
+        tela_login.classList.remove("open");
+        pageContainer.classList.remove("blur");
+    }
+
+    document.getElementById("botao_login").addEventListener("click", toggleTelaLogin);
+    pageOverlay.addEventListener("click", closeTelaLogin);
+});
